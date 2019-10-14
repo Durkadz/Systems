b9d3a3455780c248274db524a99db21d1b99d11f
Ivan <56428200+Durkadz@users.noreply.github.com>
2019-10-14 08:37:07 +0200

    Add files via upload

---
+++ Copy files.ps1
@@ -0,0 +1,14 @@
+﻿#$Original = C:\Users\ivan.redondo\Desktop\Kyocera\SDK211603\Original.csv
+#$Importacion = C:\Users\ivan.redondo\Desktop\Kyocera\SDK211603\Ruta.csv
+
+$Ori = Import-Csv C:\Users\ivan.redondo\Desktop\Kyocera\SDK209205\Original.csv 
+
+
+foreach ($Or in $Ori){
+
+$fichero = $or.ubi
+
+Copy-Item -path \\fsesbes28001004\F$\$fichero -destination \\fsesbes28001004\f$\MyDoc\BD07\SDK209205
+
+
+}
